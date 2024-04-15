fe75b5857d0cf3223cd99652b1825c030d5ba808
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-04-15 12:09:56 -0300

    chore: add .editorconfig and import.sql

---
+++ target/classes/import.sql
@@ -0,0 +1,9 @@
+insert into customers (fullname, cpf) values ('Eliza Palmer', '106.101.244-23');
+insert into customers (fullname, cpf) values ('Lester Walsh', '164.219.243-24');
+insert into customers (fullname, cpf) values ('Lelia Yates', '201.116.237-91');
+insert into customers (fullname, cpf) values ('Elizabeth Abbott', '200.46.95-39');
+
+insert into customers (fullname, email, password, cep, house_number, house_complement) values ('Clyde Stanley', 'mavku@mojje.eg', 'JtdRWqT129', '35505-531', '23', 'House');
+insert into customers (fullname, email, password, cep, house_number, house_complement) values ('Edwin Edwards', 'zemzaf@ahu.ck', 'LHNG46IzsCg500P', '75223-713', '120A', 'Apartment');
+insert into customers (fullname, email, password, cep, house_number, house_complement) values ('Dean Glover', 'pik@ruce.kz', 'vHw7jL', '43939-394', '863', 'House');
+insert into customers (fullname, email, password, cep, house_number, house_complement) values ('James Sullivan', 'va@zatpuspof.jo', 'uQPwkSqMzXID773R9U', '83285-762', '15B', 'Apartment');
