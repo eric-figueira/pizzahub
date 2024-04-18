b618449d4391e62db12696439145ca587068da5b
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-04-17 22:21:33 -0300

    feat: create RegexValidator utility

--- src/main/java/pizzahub/api/entities/Customer.java
+++ src/main/java/pizzahub/api/entities/Customer.java
@@ -5,8 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-import java.util.regex.*;
+import pizzahub.api.utils.RegexValidator;
 
 @Entity
 @Table(name = "customers")
@@ -26,10 +25,6 @@ public class Customer {
     private String cep;
     private String houseNumber;
     private String houseComplement;
-
-    private String cpfRegex = "(^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$)";
-    private String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-    private String cepRegex = "(^[0-9]{5})-?([0-9]{3}$)";
 
     public Customer() {}
 
@@ -60,7 +55,7 @@ public class Customer {
         if (cpf == null || cpf.trim().isEmpty()) {
             throw new IllegalArgumentException("[Customer]: CPF cannot be null or empty");
         }
-        if (!Pattern.matches(this.cpfRegex, cpf)) {
+        if (!RegexValidator.validateCPF(cpf)) {
             throw new IllegalArgumentException("[Customer]: CPF does not match pattern");
         }
         this.cpf = cpf;
@@ -70,7 +65,7 @@ public class Customer {
         if (email == null || email.trim().isEmpty()) {
             throw new IllegalArgumentException("[Customer]: Email cannot be null or empty");
         }
-        if (!Pattern.matches(this.emailRegex, email)) {
+        if (!RegexValidator.validateEmail(email)) {
             throw new IllegalArgumentException("[Customer]: Email does not match pattern");
         }
         this.email = email;
@@ -87,7 +82,7 @@ public class Customer {
         if (cep == null || cep.trim().isEmpty()) {
             throw new IllegalArgumentException("[Customer]: CEP cannot be null or empty");
         }
-        if (!Pattern.matches(this.cepRegex, cep)) {
+        if (!RegexValidator.validateCEP(cep)) {
             throw new IllegalArgumentException("[Customer]: CEP does not match pattern");
         }
         this.cep = cep;
---
+++ src/main/java/pizzahub/api/utils/RegexValidator.java
@@ -0,0 +1,21 @@
+package pizzahub.api.utils;
+
+import java.util.regex.*;
+
+public class RegexValidator {
+    private static String cpfRegex = "(^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$)";
+    private static String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+    private static String cepRegex = "(^[0-9]{5})-?([0-9]{3}$)";
+
+    public static boolean validateCPF(String validating) {
+        return Pattern.matches(cpfRegex, validating);
+    }
+
+    public static boolean validateCEP(String validating) {
+        return Pattern.matches(cepRegex, validating);
+    }
+
+    public static boolean validateEmail(String validating) {
+        return Pattern.matches(emailRegex, validating);
+    }
+}
