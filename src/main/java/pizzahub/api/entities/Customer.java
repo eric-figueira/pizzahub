c74de75ef9901791f14943f5518ef8498bf950e1
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-04-17 23:04:58 -0300

    chore: add lombok dependency

--- src/main/java/pizzahub/api/entities/Customer.java
+++ src/main/java/pizzahub/api/entities/Customer.java
@@ -7,8 +7,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import pizzahub.api.utils.RegexValidator;
 
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity
 @Table(name = "customers")
 public class Customer {
@@ -28,16 +38,16 @@ public class Customer {
     private String houseNumber;
     private String houseComplement;
 
-    public Customer() {}
+    // public Customer() {}
 
-    public UUID getId() { return this.id; }
-    public String getFullname() { return this.fullname; }
-    public String getCpf() { return this.cpf; }
-    public String getEmail() { return this.email; }
-    public String getPassword() { return this.password; }
-    public String getCep() { return this.cep; }
-    public String getHouseNumber() { return this.houseNumber; }
-    public String getHouseComplement() { return this.houseComplement; }
+    // public UUID getId() { return this.id; }
+    // public String getFullname() { return this.fullname; }
+    // public String getCpf() { return this.cpf; }
+    // public String getEmail() { return this.email; }
+    // public String getPassword() { return this.password; }
+    // public String getCep() { return this.cep; }
+    // public String getHouseNumber() { return this.houseNumber; }
+    // public String getHouseComplement() { return this.houseComplement; }
 
     public void setId(UUID id) throws Exception {
         if (id.toString().isEmpty()) {
@@ -105,54 +115,54 @@ public class Customer {
         this.houseComplement = houseComplement;
     }
 
-    @Override
-    public int hashCode ()
-    {
-        int ret = 23,
-            k = 11;
+    // @Override
+    // public int hashCode ()
+    // {
+    //     int ret = 23,
+    //         k = 11;
 
-        ret += k * ret + this.id.hashCode();
-        ret += k * ret + String.valueOf(this.fullname).hashCode();
-        ret += k * ret + String.valueOf(this.cpf).hashCode();
-        ret += k * ret + String.valueOf(this.email).hashCode();
-        ret += k * ret + String.valueOf(this.password).hashCode();
-        ret += k * ret + String.valueOf(this.cep).hashCode();
-        ret += k * ret + String.valueOf(this.houseNumber).hashCode();
-        ret += k * ret + String.valueOf(this.houseComplement).hashCode();
+    //     ret += k * ret + this.id.hashCode();
+    //     ret += k * ret + String.valueOf(this.fullname).hashCode();
+    //     ret += k * ret + String.valueOf(this.cpf).hashCode();
+    //     ret += k * ret + String.valueOf(this.email).hashCode();
+    //     ret += k * ret + String.valueOf(this.password).hashCode();
+    //     ret += k * ret + String.valueOf(this.cep).hashCode();
+    //     ret += k * ret + String.valueOf(this.houseNumber).hashCode();
+    //     ret += k * ret + String.valueOf(this.houseComplement).hashCode();
 
-        if (ret < 0)
-            ret -= ret;
+    //     if (ret < 0)
+    //         ret -= ret;
 
-        return ret;
-    }
+    //     return ret;
+    // }
 
-    public boolean equals (Customer other)
-    {
-        if (this == other) return true;
-        if (other == null) return false;
+    // public boolean equals (Customer other)
+    // {
+    //     if (this == other) return true;
+    //     if (other == null) return false;
 
-        if (this.id != other.id) return false;
-        if (!this.fullname.equals(other.fullname)) return false;
-        if (!this.cpf.equals(other.cpf)) return false;
-        if (!this.email.equals(other.email)) return false;
-        if (!this.password.equals(other.password)) return false;
-        if (!this.cep.equals(other.cep)) return false;
-        if (!this.houseNumber.equals(other.houseNumber)) return false;
-        if (!this.houseComplement.equals(other.houseComplement)) return false;
+    //     if (this.id != other.id) return false;
+    //     if (!this.fullname.equals(other.fullname)) return false;
+    //     if (!this.cpf.equals(other.cpf)) return false;
+    //     if (!this.email.equals(other.email)) return false;
+    //     if (!this.password.equals(other.password)) return false;
+    //     if (!this.cep.equals(other.cep)) return false;
+    //     if (!this.houseNumber.equals(other.houseNumber)) return false;
+    //     if (!this.houseComplement.equals(other.houseComplement)) return false;
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    @Override
-    public String toString ()
-    {
-        return "Id: " + this.id.toString() + " - " +
-            "Fullname: " + this.fullname + " - " +
-            "CPF: " + this.cpf + " - " +
-            "Email: " + this.email + " - " +
-            "Password: " + this.password + " - " +
-            "CEP: " + this.cep + " - " +
-            "House Number: " + this.houseNumber + " - " +
-            "House Complement: " + this.houseComplement + " - ";
-    }
+    // @Override
+    // public String toString ()
+    // {
+    //     return "Id: " + this.id.toString() + " - " +
+    //         "Fullname: " + this.fullname + " - " +
+    //         "CPF: " + this.cpf + " - " +
+    //         "Email: " + this.email + " - " +
+    //         "Password: " + this.password + " - " +
+    //         "CEP: " + this.cep + " - " +
+    //         "House Number: " + this.houseNumber + " - " +
+    //         "House Complement: " + this.houseComplement + " - ";
+    // }
 }
